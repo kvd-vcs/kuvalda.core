6774daddf12672244151adabcb9c642c44e41937
Stanislav Petrov <kodep@kodep.pro>
2018-11-01 16:40:30 +0300

    Add project files.

---
+++ Kuvalda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kuvalda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
