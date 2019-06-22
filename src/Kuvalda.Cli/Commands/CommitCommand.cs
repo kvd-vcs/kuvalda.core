84b92d8f5a9cce9f934d5d02fe3dcaeb154d300e
Stanislav Petrov <sv.petrov@kodep.pro>
2019-06-22 16:00:31 +0300

    move commands to folder

---
+++ src/Kuvalda.Cli/Commands/CommitCommand.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Kuvalda.Core;
+
+namespace Kuvalda.Cli
+{
+    public class CommitCommand : ICliCommand
+    {
+        private readonly IRepositoryFacade _repositoryFacade;
+        private readonly ApplicationInstanceSettings _applicationInstanceSettings;
+
+        public CommitCommand(IRepositoryFacade repositoryFacade, ApplicationInstanceSettings applicationInstanceSettings)
+        {
+            _repositoryFacade = repositoryFacade;
+            _applicationInstanceSettings = applicationInstanceSettings;
+        }
+
+        public async Task<int> Execute(string[] args)
+        {
+            var message = args.Skip(1).FirstOrDefault();
+            if (string.IsNullOrEmpty(message))
+            {
+                message = "no message";
+            }
+
+            var chash = await _repositoryFacade.Commit(new CommitOptions(_applicationInstanceSettings.RepositoryPath, message));
+
+            Console.WriteLine(chash);
+            
+            return 0;
+        }
+    }
+}
