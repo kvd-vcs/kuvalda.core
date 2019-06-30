b233d2919e9dd133e9b0ae33dccb3a258c9a482a
Stanislav Petrov <sv.petrov@kodep.pro>
2019-06-30 15:39:14 +0300

    added assembly info file

---
+++ src/Kuvalda.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyCompanyAttribute("Petrov Stanislav <sv.petrov@kodep.pro>")]
+[assembly: AssemblyCopyrightAttribute("Copyright (c) 2019 Petrov Stanislav (sv.petrov@kodep.pro)")]
+[assembly: AssemblyDescriptionAttribute("This package includes Kuvalda.Core and related assemblies")]
+[assembly: AssemblyProductAttribute("Kuvalda.Core")]
+[assembly: AssemblyTitleAttribute("Kuvalda.Core")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
