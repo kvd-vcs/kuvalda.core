4d28d49b83abc4cd64f3ac356cac77aea093ed6c
Stanislav Petrov <sv.petrov@kodep.pro>
2019-07-17 00:58:52 +0300

    tree merge conflict service logic improved + added more unit tests

--- src/Kuvalda.Core/Merge/ConflictDetectService.cs
+++ src/Kuvalda.Core/Merge/ConflictDetectService.cs
@@ -7,10 +7,12 @@ namespace Kuvalda.Core.Merge
     public class ConflictDetectService : IConflictDetectService
     {
         private readonly IDifferenceEntriesCreator _entriesCreator;
+        private readonly IFlatTreeCreator _flatTreeCreator;
 
-        public ConflictDetectService(IDifferenceEntriesCreator entriesCreator)
+        public ConflictDetectService(IDifferenceEntriesCreator entriesCreator, IFlatTreeCreator flatTreeCreator)
         {
             _entriesCreator = entriesCreator ?? throw new ArgumentNullException(nameof(entriesCreator));
+            _flatTreeCreator = flatTreeCreator ?? throw new ArgumentNullException(nameof(flatTreeCreator));
         }
 
         public IEnumerable<MergeConflict> Detect(TreeNode baseTree, TreeNode leftTree, TreeNode rightTree)
@@ -19,15 +21,45 @@ namespace Kuvalda.Core.Merge
             var rightDiff = _entriesCreator.Create(baseTree, rightTree);
 
             var modifiedConflict = leftDiff.Modified.Intersect(rightDiff.Modified);
-            var addedRemovedConflict = leftDiff.Added.Intersect(rightDiff.Removed)
-                .Union(leftDiff.Removed.Intersect(rightDiff.Added));
-            var addedConflicts = leftDiff.Added.Intersect(rightDiff.Added);
+            var addedRemovedLeft = leftDiff.Added.Intersect(rightDiff.Removed);
+            var addedRemovedRight = leftDiff.Removed.Intersect(rightDiff.Added);
+            var bothAdded = leftDiff.Added.Intersect(rightDiff.Added);
 
-            var conflicts = modifiedConflict.Union(addedRemovedConflict)
-                .Union(addedConflicts)
-                .ToArray();
+            if (bothAdded.Any() || modifiedConflict.Any())
+            {
+                var leftFlat = _flatTreeCreator.Create(leftTree).ToDictionary(i => i.Name, i => i.Node);
+                var rightFlat = _flatTreeCreator.Create(rightTree).ToDictionary(i => i.Name, i => i.Node);
 
-            return null;
+                bothAdded = bothAdded.Where(i => IsHashConflict(i, leftFlat, rightFlat));
+                modifiedConflict = modifiedConflict.Where(i => IsHashConflict(i, leftFlat, rightFlat));
+            }
+
+            return modifiedConflict
+                .Select(i => new MergeConflict(i, MergeConflictReason.Modify, MergeConflictReason.Modify))
+                .Union(addedRemovedLeft.Select(i =>
+                    new MergeConflict(i, MergeConflictReason.Added, MergeConflictReason.Removed)))
+                .Union(addedRemovedRight.Select(i =>
+                    new MergeConflict(i, MergeConflictReason.Removed, MergeConflictReason.Added)))
+                .Union(bothAdded.Select(i =>
+                    new MergeConflict(i, MergeConflictReason.Added, MergeConflictReason.Added)));
+        }
+
+        private bool IsHashConflict(string s, IDictionary<string, TreeNode> leftFlat, IDictionary<string, TreeNode> rightFlat)
+        {
+            var left = leftFlat[s];
+            var right = rightFlat[s];
+
+            if (left.GetType() != right.GetType())
+            {
+                return true;
+            }
+
+            if (left is TreeNodeFile leftFile && right is TreeNodeFile rightFile)
+            {
+                return !string.Equals(leftFile.Hash, rightFile.Hash);
+            }
+
+            return false;
         }
     }
 }
--- src/Kuvalda.Core/Merge/MergeConflict.cs
+++ src/Kuvalda.Core/Merge/MergeConflict.cs
@@ -12,5 +12,29 @@ namespace Kuvalda.Core.Merge
             LeftReason = leftReason;
             RightReason = rightReason;
         }
+
+        protected bool Equals(MergeConflict other)
+        {
+            return string.Equals(Path, other.Path) && LeftReason == other.LeftReason && RightReason == other.RightReason;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((MergeConflict) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Path != null ? Path.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (int) LeftReason;
+                hashCode = (hashCode * 397) ^ (int) RightReason;
+                return hashCode;
+            }
+        }
     }
 }
